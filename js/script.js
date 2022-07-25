0a8785848780b2614214b28d06adb4a4e4a08d82
Noak-Sergey <noakstuff@gmail.com>
2022-07-25 12:55:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+   const menu = document.querySelector('.nav-menu'),
+   hamburger = document.querySelector('.hamburger'),
+   menuItem = document.querySelectorAll('.nav-menu__list-item');
+
+   hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('nav-menu_active');
+   });
+
+   menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+
+})
+
